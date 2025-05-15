3fc3c8aa6d44118d426d1b75b400dbb012ada4ab
agkayster <ejike.chibok@gmail.com>
2025-05-15 15:59:06 +0100

    plot a single columns and rest the index to use it on the horizontal axis

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -12,7 +12,16 @@ df = pd.read_pickle("../../data/interim/01_data_processed.pkl")
 # --------------------------------------------------------------
 # Plot single columns
 # --------------------------------------------------------------
+# create a data frame for a specific set
+# create a subset of the original data frame using the set number from the set column
+set_df = df[df["set"] == 64]
 
+# plots our accelerometer data for the x-axis as against time on the y-axis
+plt.plot(set_df["acc_x"])
+
+# this is using the reset_index method to reset the index of the DataFrame
+# and we use the index as our horizontal axis, which tells us that there are more than 80 samples
+plt.plot(set_df["acc_x"].reset_index(drop=True))
 
 # --------------------------------------------------------------
 # Plot all exercises
