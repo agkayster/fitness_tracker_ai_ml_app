732dbd3ac9f8796565a8b2651d47339ab676daa8
agkayster <ejike.chibok@gmail.com>
2025-05-29 15:04:28 +0100

    Implement mark_outliers_chauvenet function, plot histogram for all oultier columns and loop over all columns using the mark_oultiers_chauvenet's function

--- src/features/remove_outliers.py
+++ src/features/remove_outliers.py
@@ -147,13 +147,77 @@ for col in outlier_columns:
 
 # Check for normal distribution
 
+# we pass in the "outlier_columns" below
+# this is for accelerometer data
+# replace "boxplot" with "plot.hist"
+df[outlier_columns[:3] + ["label"]].plot.hist(
+    by="label", figsize=(20, 20), fontsize=14, layout=(3, 3)
+)
+
+# we pass in the outlier columns below
+# this is for gyroscope data
+# replace "boxplot" with "plot.hist"
+df[outlier_columns[3:] + ["label"]].plot.hist(
+    by="label", figsize=(20, 20), fontsize=14, layout=(3, 3)
+)
+
 
 # Insert Chauvenet's function
+def mark_outliers_chauvenet(dataset, col, C=2):
+    """Finds outliers in the specified column of datatable and adds a binary column with
+    the same name extended with '_outlier' that expresses the result per data point.
+
+    Taken from: https://github.com/mhoogen/ML4QS/blob/master/Python3Code/Chapter3/OutlierDetection.py
+
+    Args:
+        dataset (pd.DataFrame): The dataset
+        col (string): The column you want apply outlier detection to
+        C (int, optional): Degree of certainty for the identification of outliers given the assumption
+                           of a normal distribution, typicaly between 1 - 10. Defaults to 2.
+
+    Returns:
+        pd.DataFrame: The original dataframe with an extra boolean column
+        indicating whether the value is an outlier or not.
+    """
+
+    dataset = dataset.copy()
+    # Compute the mean and standard deviation.
+    mean = dataset[col].mean()
+    std = dataset[col].std()
+    N = len(dataset.index)
+    criterion = 1.0 / (C * N)
+
+    # Consider the deviation for the data points.
+    deviation = abs(dataset[col] - mean) / std
+
+    # Express the upper and lower bounds.
+    low = -deviation / math.sqrt(C)
+    high = deviation / math.sqrt(C)
+    prob = []
+    mask = []
+
+    # Pass all rows in the dataset.
+    for i in range(0, len(dataset.index)):
+        # Determine the probability of observing the point
+        prob.append(
+            1.0 - 0.5 * (scipy.special.erf(high[i]) - scipy.special.erf(low[i]))
+        )
+        # And mark as an outlier when the probability is below our criterion.
+        mask.append(prob[i] < criterion)
+    dataset[col + "_outlier"] = mask
+    return dataset
 
 
 # Loop over all columns
-
-
+for col in outlier_columns:
+    # for all outlier_columns(acc_x, acc_y, acc_z, gyro_x, gyro_y, gyro_z)
+    # first run the mark_outliers_iqr function
+    # store the function in the dataset
+    # then plot the binary outliers (6 times)
+    dataset = mark_outliers_chauvenet(df, col, C=2)
+    plot_binary_outliers(
+        dataset=dataset, col=col, outlier_col=col + "_outlier", reset_index=True
+    )
 # --------------------------------------------------------------
 # Local outlier factor (distance based)
 # --------------------------------------------------------------
