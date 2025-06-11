deaf42dc97f9357caef6e13b9fedfab0ba18407d
agkayster <ejike.chiboka@gmail.com>
2025-06-11 23:03:28 +0100

    Implemented the average duration of a set

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -37,6 +37,33 @@ df.info()
 # Calculating set duration
 # --------------------------------------------------------------
 
+df[df["set"] == 25]["acc_y"].plot()
+
+# here we can calculate the duration of a set
+# if you subtract 2 timestamps you get a time delta variable
+duration = df[df["set"] == 1].index[-1] - df[df["set"] == 1].index[0]
+
+# we can call the seconds
+duration.seconds
+
+# use the loop to calculate the average duration of all sets
+# s means each individual set number or id
+for s in df["set"].unique():
+    start = df[df["set"] == s].index[0]
+    stop = df[df["set"] == s].index[-1]
+
+    duration = stop - start
+    # adds our duration column to the data table
+    df.loc[(df["set"] == s), "duration"] = duration.seconds
+
+# df shows our new table with the duration column
+# we group by category and take the average duration of each category
+duration_df = df.groupby(["category"])["duration"].mean()
+
+# average duration per repetition
+duration_df.iloc[0] / 5 # because it was repeated 5 times
+duration_df.iloc[1] / 10 # medium set was repeated 10 times
+
 
 # --------------------------------------------------------------
 # Butterworth lowpass filter
